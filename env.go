c2fc805f31f50569a171005eadb8678b076ef64a
Fernando Fragateiro <Fernando.fragateiro@emma-sleep.com>
2025-04-06 19:33:49 +0100

    Add fix env

--- env.go
+++ env.go
@@ -26,24 +26,27 @@ type MateoMaticsSecrets struct {
 func LoadEnvKey() (weatherServiceKeys *WeatherServiceKeys) {
 	weatherServiceKeys = &WeatherServiceKeys{}
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file", err.Error())
+	// Load .env file only in development
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Error loading .env file", err.Error())
+		}
 	}
 
 	weatherServiceKeys.OpenWeatherAPIKey = os.Getenv("WEATHER_API_KEY")
 
-	weatherServiceKeys.StormGlassAPIKey = os.Getenv("STORMGLASS_API_KEY")
+	//weatherServiceKeys.StormGlassAPIKey = os.Getenv("STORMGLASS_API_KEY")
 
-	weatherServiceKeys.MateoMaticsAuths.Username = os.Getenv("MATEOMATICS_USERNAME")
-	weatherServiceKeys.MateoMaticsAuths.Password = os.Getenv("MATEOMATICS_PASSWORD")
+	//weatherServiceKeys.MateoMaticsAuths.Username = os.Getenv("MATEOMATICS_USERNAME")
+	//	weatherServiceKeys.MateoMaticsAuths.Password = os.Getenv("MATEOMATICS_PASSWORD")
 
 	weatherServiceKeys.YoutubeAPIKey = os.Getenv("YOUTUBE_API_KEY")
 
-	weatherServiceKeys.MarkcorpsAPIKey = os.Getenv("MARKCORPS_API_KEY")
+	//	weatherServiceKeys.MarkcorpsAPIKey = os.Getenv("MARKCORPS_API_KEY")
 
-	weatherServiceKeys.AmadeusID = os.Getenv("AMADEUS_ID")
-	weatherServiceKeys.AmadeusSecret = os.Getenv("AMADEUS_CLIENT_SECRET")
+	//weatherServiceKeys.AmadeusID = os.Getenv("AMADEUS_ID")
+	//weatherServiceKeys.AmadeusSecret = os.Getenv("AMADEUS_CLIENT_SECRET")
 
 	weatherServiceKeys.GooglePlacesAPIKey = os.Getenv("GOOGLE_PLACES_API_KEY")
 
