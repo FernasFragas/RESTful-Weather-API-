dce7ec5f4b21457fd3f83ac6593eda3eca17776b
Fernando Fragateiro <Fernando.fragateiro@emma-sleep.com>
2025-08-05 14:23:00 +0100

    Fix Database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,7 +27,7 @@ func main() {
 	)
 
 	server := weatherservice.NewAppServer(reporters, videoStreamReporters, hotelsApi)
-	if err := server.InitializeDatabase("weatherservice.db"); err != nil {
+	if err := server.InitializeDatabase("/data/weatherservice.db"); err != nil {
 		log.Fatal(err)
 	}
 
