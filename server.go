03983b3d23e4074ab930093f318b5ffe2dfa0ec2
Fernando Fragateiro <Fernando.fragateiro@emma-sleep.com>
2026-01-05 22:25:29 +0000

    Remove itinerary feature

--- server.go
+++ server.go
@@ -131,11 +131,10 @@ func (s *Server) listGeneralInfo(ctx *fiber.Ctx) error {
 
 	// Render the full page for regular requests
 	return ctx.Render("index", fiber.Map{
-		"Query":          city,
-		"GeneralInfo":    data.GeneralInfo,
-		"Videos":         data.Videos,
-		"Hotels":         data.Hotels,
-		"ItineraryItems": data.ItineraryItems,
+		"Query":       city,
+		"GeneralInfo": data.GeneralInfo,
+		"Videos":      data.Videos,
+		"Hotels":      data.Hotels,
 	})
 }
 
